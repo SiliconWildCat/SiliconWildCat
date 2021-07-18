4d6354170b7acea8b94ea245bdfdc824577d6382
jieun1128 <5656jieun@naver.com>
2021-07-18 16:11:51 +0900

    fix : mysql 연결을 gcp mysql로 설정

--- backend/web/config.py
+++ backend/web/config.py
@@ -1,9 +1,9 @@
 db = {
-    'user'     : 'root',
-    'password' : '1234',
-    'host'     : 'mysql_db',
-    'port'     : '3306',
-    'database' : 'app'
+    'user'     : '',
+    'password' : '',
+    'host'     : '',
+    'port'     : '',
+    'database' : ''
 }
 
 DB_URL = f"mysql+mysqlconnector://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['database']}?charset=utf8"
