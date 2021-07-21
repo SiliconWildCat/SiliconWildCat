d60e566827e67a00d0795f70ad7327ac21826e8a
ww8007 <wshmin@naver.com>
2021-07-21 22:29:18 +0900

    feat : sass option 추가

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /.(png|jpg)$/,
@@ -15,11 +18,29 @@ module.exports = {
     });
     return config;
   },
-  // exportPathMap: function () {
-  //   return {
-  //     '/page1': { page: '/page1' },
-  //     '/page2-hello': { page: '/page2', query: { text: 'hello' } },
-  //     '/page2-world': { page: '/page2', query: { text: 'world' } },
-  //   };
-  // },
 };
+// module.exports = {
+//   webpack: (config) => {
+//     config.module.rules.push({
+//       test: /.(png|jpg)$/,
+//       use: [
+//         {
+//           loader: 'file-loader',
+//           options: {
+//             name: '[path][name].[ext]?[hash]',
+//             emitFile: false,
+//             publicPath: '/',
+//           },
+//         },
+//       ],
+//     });
+//     return config;
+//   },
+// exportPathMap: function () {
+//   return {
+//     '/page1': { page: '/page1' },
+//     '/page2-hello': { page: '/page2', query: { text: 'hello' } },
+//     '/page2-world': { page: '/page2', query: { text: 'world' } },
+//   };
+// },
+//};
