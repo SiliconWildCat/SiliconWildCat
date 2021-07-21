72992ba858d9ff632e52ce789a06542796255590
ww8007 <wshmin@naver.com>
2021-07-22 00:58:38 +0900

    feat : withRedxuSaga 를 이용한 App wrapping

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,8 @@
 import { AppProps } from 'next/app';
 import wrapper from '../store';
+import withReduxSaga from 'next-redux-saga';
 function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
-export default wrapper.withRedux(App);
+export default wrapper.withRedux(withReduxSaga(App));
