50891d18d3f7956d5239cc9fa6345fc6ccf2d488
Dudu-CNU <k0s0a7@gmail.com>
2021-07-22 21:38:35 +0900

    design : import player css
    Co-authored-by: 장동현 <ww8007@users.noreply.github.com>

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 import wrapper from '../store';
 import withReduxSaga from 'next-redux-saga';
+import "../styles/playerStyles.css";
 function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
