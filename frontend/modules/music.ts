ba4604e96ca4500a9c0ce55e0e35ceca62a32b4e
ww8007 <wshmin@naver.com>
2021-07-23 15:07:33 +0900

    feat : slides img 정보 담고 있는 url 추가

---
+++ frontend/modules/music.ts
@@ -0,0 +1,34 @@
+import img1 from '../images/1.jpeg';
+import img2 from '../images/2.jpeg';
+import img3 from '../images/3.jpeg';
+import img4 from '../images/4.jpeg';
+export const slides = [
+  {
+    city: 'Paris',
+    country: 'France',
+    img: `https://ww8007bucket.s3.ap-northeast-2.amazonaws.com/images/1.jpeg`,
+  },
+
+  {
+    city: 'Singapore',
+    img: 'https://ww8007bucket.s3.ap-northeast-2.amazonaws.com/images/2.jpeg',
+  },
+
+  {
+    city: 'Prague',
+    country: 'Czech Republic',
+    img: 'https://ww8007bucket.s3.ap-northeast-2.amazonaws.com/images/3.jpeg',
+  },
+
+  {
+    city: 'Amsterdam',
+    country: 'Netherlands',
+    img: 'https://ww8007bucket.s3.ap-northeast-2.amazonaws.com/images/6.jpeg',
+  },
+
+  {
+    city: 'Moscow',
+    country: 'Russia',
+    img: 'https://ww8007bucket.s3.ap-northeast-2.amazonaws.com/images/7.jpeg',
+  },
+];
