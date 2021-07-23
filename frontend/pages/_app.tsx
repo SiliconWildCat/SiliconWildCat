a2bdda381212e7e3607d00806d4e6eae27b8d576
ww8007 <wshmin@naver.com>
2021-07-23 15:08:15 +0900

    style : add music animation
    Co-authored-by: Seonghan Kim, Dudu <Dudu-CNU@users.noreply.github.com>

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,8 @@
 import { AppProps } from 'next/app';
 import wrapper from '../store';
 import withReduxSaga from 'next-redux-saga';
-import "../styles/playerStyles.css";
+import '../styles/playerStyles.css';
+import '../components/Music/music.scss';
 function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
