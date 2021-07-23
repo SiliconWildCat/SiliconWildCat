18593a994d84cedf73696dedda486939b9176077
ww8007 <wshmin@naver.com>
2021-07-24 01:23:22 +0900

    desgin : scss 파일들 추가

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,10 @@
 import { AppProps } from 'next/app';
 import wrapper from '../store';
 import withReduxSaga from 'next-redux-saga';
-import '../styles/playerStyles.css';
+// import '../styles/playerStyles.css';
 import '../components/Music/music.scss';
+import '../components/TTS/tts.scss';
+import 'react-h5-audio-player/lib/styles.css';
 function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
