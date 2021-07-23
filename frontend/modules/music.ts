e08dbed0ae4992ee64776adeb2b8a56fdbf1b248
ww8007 <wshmin@naver.com>
2021-07-23 18:53:27 +0900

    fix : music-title

--- frontend/modules/music.ts
+++ frontend/modules/music.ts
@@ -5,30 +5,31 @@ import img4 from '../images/4.jpeg';
 export const slides = [
   {
     city: 'Paris',
-    country: 'France',
+    country: '제주도 푸른밤',
     img: `https://ww8007bucket.s3.ap-northeast-2.amazonaws.com/images/1.jpeg`,
   },
 
   {
     city: 'Singapore',
+    country: '별 보러 갈래',
     img: 'https://ww8007bucket.s3.ap-northeast-2.amazonaws.com/images/2.jpeg',
   },
 
   {
     city: 'Prague',
-    country: 'Czech Republic',
+    country: '그리 쉽게 이별을 말하지 말아요',
     img: 'https://ww8007bucket.s3.ap-northeast-2.amazonaws.com/images/3.jpeg',
   },
 
   {
     city: 'Amsterdam',
-    country: 'Netherlands',
+    country: 'Stay',
     img: 'https://ww8007bucket.s3.ap-northeast-2.amazonaws.com/images/6.jpeg',
   },
 
   {
     city: 'Moscow',
-    country: 'Russia',
+    country: 'Good',
     img: 'https://ww8007bucket.s3.ap-northeast-2.amazonaws.com/images/7.jpeg',
   },
 ];
