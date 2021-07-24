473a028c83f9a2dc5829980ef527db8e4bf2f857
ww8007 <wshmin@naver.com>
2021-07-24 18:20:13 +0900

    feat : musicSlice 추가 음악 제목과 imgURL에 대한 변경사항 수정 changeSelect action 추가

--- frontend/modules/music.ts
+++ frontend/modules/music.ts
@@ -1,35 +1,42 @@
-import img1 from '../images/1.jpeg';
-import img2 from '../images/2.jpeg';
-import img3 from '../images/3.jpeg';
-import img4 from '../images/4.jpeg';
-export const slides = [
-  {
-    city: 'Paris',
-    country: '제주도 푸른밤',
-    img: `https://t1.daumcdn.net/cfile/tistory/9975134C5C97724E08`,
-  },
+import { IMusic } from '../interface/music';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-  {
-    city: 'Singapore',
-    country: '별 보러 갈래',
-    img: 'https://t1.daumcdn.net/cfile/tistory/994D5C485C8EEF791E?original',
-  },
+export const initialState: IMusic = {
+  selectNum: 1,
+  musics: [
+    {
+      title: '제주도 푸른밤',
+      imgURL: 'https://t1.daumcdn.net/cfile/tistory/9975134C5C97724E08',
+    },
 
-  {
-    city: 'Prague',
-    country: '그리 쉽게 이별을 말하지 말아요',
-    img: 'https://t1.daumcdn.net/cfile/tistory/9980B4485C8EEF781C',
-  },
+    {
+      title: '별 보러 갈래',
+      imgURL:
+        'https://t1.daumcdn.net/cfile/tistory/994D5C485C8EEF791E?original',
+    },
 
-  {
-    city: 'Amsterdam',
-    country: 'Stay',
-    img: 'https://t1.daumcdn.net/cfile/tistory/9954D9485C8EEF7C1E',
-  },
+    {
+      title: '그리 쉽게 이별을 말하지 말아요',
+      imgURL: 'https://t1.daumcdn.net/cfile/tistory/9980B4485C8EEF781C',
+    },
 
-  // {
-  //   city: 'Moscow',
-  //   country: 'Good',
-  //   img: 'https://ww8007bucket.s3.ap-northeast-2.amazonaws.com/images/7.jpeg',
-  // },
-];
+    {
+      title: 'Stay',
+      imgURL: 'https://t1.daumcdn.net/cfile/tistory/9954D9485C8EEF7C1E',
+    },
+  ],
+};
+
+export const musicSlice = createSlice({
+  name: 'music',
+  initialState: initialState,
+  reducers: {
+    changeSelect: (state, action: PayloadAction<number>) => {
+      state.selectNum = action.payload;
+    },
+  },
+});
+
+export const { changeSelect } = musicSlice.actions;
+
+export default musicSlice.reducer;
