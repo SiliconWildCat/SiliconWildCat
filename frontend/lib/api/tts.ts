6247f6434205f7c52b6a840f9fc8482a34498f7d
ww8007 <wshmin@naver.com>
2021-07-24 22:32:42 +0900

    fix : postText data 추가

--- frontend/lib/api/tts.ts
+++ frontend/lib/api/tts.ts
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-export const postText = () =>
-  axios.post(`https://jsonplaceholder.typicode.com/posts/`);
+export const postText = ({ text }) =>
+  axios.post(
+    `https://jsonplaceholder.typicode.com/posts/`,
+    JSON.stringify(text)
+  );
 export const getUsers = () =>
   axios.get(`https://jsonplaceholder.typicode.com/users`);
