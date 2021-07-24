b61f9556e8e3e4e3fe8e55e27b4001a71c30b229
ww8007 <wshmin@naver.com>
2021-07-24 23:55:04 +0900

    fix : homepage error

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,7 +3,6 @@ import Head from 'next/head';
 import Link from 'next/link';
 import styled from 'styled-components';
 import Counter from '../components/Counter';
-import TTS from '../components/Tts';
 import MusicSlider from '../components/Music/MusicSlider';
 import ToggleMenu from '../components/Music/ToggleMenu';
 import MusicPlayer from '../components/musicPlayer';
@@ -11,10 +10,6 @@ import MusicPlayer from '../components/musicPlayer';
 export default function Home() {
   return (
     <div className="container">
-      {/* <TTS /> 
-      <Link href="/tts"></Link>
-      <Link href="/music"></Link>
-       */}
       <ToggleMenu />
       <MusicSlider />
       <MusicPlayer />
