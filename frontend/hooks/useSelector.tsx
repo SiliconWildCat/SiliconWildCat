1c4f4f69682af097091af60769edc5bc2a953fcf
ww8007 <wshmin@naver.com>
2021-07-24 18:24:01 +0900

    feat : useAppDispatch 함수 추가 dispatch를 바로 가능하게 해줌

--- frontend/hooks/useSelector.tsx
+++ frontend/hooks/useSelector.tsx
@@ -1,4 +1,6 @@
-import { TypedUseSelectorHook, useSelector } from 'react-redux';
-import { RootState } from '../store';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import type { RootState, AppDispatch } from '../store';
 
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
