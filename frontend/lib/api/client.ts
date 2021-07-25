7a8f861db4527c33bb5159d6c1f4eeb4635a4b35
ww8007 <wshmin@naver.com>
2021-07-25 16:30:27 +0900

    fix : baseURL

--- frontend/lib/api/client.ts
+++ frontend/lib/api/client.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const client = axios.create();
 
-client.defaults.baseURL = 'http://52.79.63.132:8000/';
+client.defaults.baseURL = 'http://localhost:8000/';
 
 // client.defaults.baseURL = '';
 client.defaults.withCredentials = true;
