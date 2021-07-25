943efaff7cc90d1b33dbe6614e8fe9e2afb47dde
thfla1105 <lsr_1105@naver.com>
2021-07-25 22:21:30 +0900

    fix: tts.ts 헤더 수정

--- frontend/lib/api/tts.ts
+++ frontend/lib/api/tts.ts
@@ -2,8 +2,15 @@ import axios from 'axios';
 import { submit } from '../../modules/tts';
 import client from './client';
 export const submitTTS = ({ text, type }: submit) => {
-  const data = JSON.stringify({ speech: text, voices: type });
-  return client.post(`/TTS`, data);
+  const headers = {
+    'Content-type': 'application/json; charset=UTF-8',
+     Accept: '*/*',
+  };
+
+  const data = JSON.stringify({ "speech": text, "voices": type });
+  return client.post(`/TTS`,data, {headers});
 };
+
 export const getUsers = () =>
   axios.get(`https://jsonplaceholder.typicode.com/users`);
+
