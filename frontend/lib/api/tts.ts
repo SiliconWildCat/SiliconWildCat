7033a1e98d6a305a1df7f02d509f8aa5177a2431
ww8007 <wshmin@naver.com>
2021-07-25 17:39:34 +0900

    fix : url 주소 변경

--- frontend/lib/api/tts.ts
+++ frontend/lib/api/tts.ts
@@ -3,7 +3,7 @@ import { submit } from '../../modules/tts';
 import client from './client';
 export const submitTTS = ({ text, type }: submit) => {
   const data = JSON.stringify({ speech: text, voices: type });
-  return client.post(`/users`, data);
+  return client.post(`/TTS`, data);
 };
 export const getUsers = () =>
   axios.get(`https://jsonplaceholder.typicode.com/users`);
