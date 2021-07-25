c07ea83ce73204dc99c00bcba473e5632a24d05d
ww8007 <wshmin@naver.com>
2021-07-26 00:07:30 +0900

    fix : path

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 import Counter from '../components/Counter';
 import MusicSlider from '../components/Music/MusicSlider';
 import ToggleMenu from '../components/Music/ToggleMenu';
-import MusicPlayer from '../components/MusicPlayer';
+import MusicPlayer from '../components/MusicPlayer2';
 
 export default function Home() {
   return (
