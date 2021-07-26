c4afda133f4973decea6d91f40efb549acacf4dc
ww8007 <wshmin@naver.com>
2021-07-27 01:43:02 +0900

    fix : SUBMIT_TTS

--- frontend/modules/tts.ts
+++ frontend/modules/tts.ts
@@ -4,7 +4,8 @@ import * as api from '../lib/api/tts';
 import createRequestSaga from '../hooks/createRequestSaga';
 import { takeLatest } from 'redux-saga/effects';
 import { createAction } from 'redux-actions';
-
+import { stat } from 'fs';
+import axios from 'axios';
 export const tts = [
   {
     city: 'Paris',
@@ -17,7 +18,6 @@ export const tts = [
     img: `https://ww8007bucket.s3.ap-northeast-2.amazonaws.com/%ED%83%9C%EC%97%B0/%E1%84%89%E1%85%B3%E1%84%8F%E1%85%B3%E1%84%85%E1%85%B5%E1%86%AB%E1%84%89%E1%85%A3%E1%86%BA+2021-07-25+%E1%84%8B%E1%85%A9%E1%84%92%E1%85%AE+4.16.36.png`,
   },
 ];
-
 const initialState: ITts = {
   text: '',
   mp3File: '',
@@ -25,29 +25,20 @@ const initialState: ITts = {
   error: '',
   mp3File2: '',
 };
-
 export interface submit {
   text: string;
   type: string;
 }
 const SUBMIT_TTS = 'tts/SUBMIT_TTS';
 const GET_TTS = 'tts/GET_TTS';
-const GET_MP3 = 'tts/GET_MP3';
-
 export const submitTTS = createAction(SUBMIT_TTS, (info: submit) => info);
 export const getTTS = createAction(GET_TTS);
-// export const getMP3 = createAction(GET_MP3);
-
 const submitTTSSaga = createRequestSaga(SUBMIT_TTS, api.submitTTS);
 const getTTSSaga = createRequestSaga(GET_TTS, api);
-// const getMP3Saga = createRequestSaga(GET_MP3, api.getMP3);
-
 export function* ttsSaga() {
   yield takeLatest(SUBMIT_TTS, submitTTSSaga);
   yield takeLatest(GET_TTS, getTTSSaga);
-  // yield takeLatest(GET_MP3, getMP3Saga);
 }
-
 export const ttsSlice = createSlice({
   name: 'tts',
   initialState: initialState,
@@ -62,30 +53,21 @@ export const ttsSlice = createSlice({
       state.type = action.payload;
     },
     SUBMIT_TTS_SUCCESS: (state, action: PayloadAction<any>) => {
-      const blob = new Blob([action.payload], {
-        type: 'audio/wav',
-      });
-
-      const myURL = URL.createObjectURL(blob);
-      state.mp3File2 = myURL;
+      //console.log(action.payload);
+      //var array_data=action.payload;
+      //var blob=new Blob([array_data],{ 'type' : 'audio/wav' });
+      //var url= URL.createObjectURL(blob);
+      var url = action.payload['data'];
+      state.mp3File = 'http://localhost' + url;
     },
-    // GET_MP3_SUCCESS: (state, action: PayloadAction<any>) => {
-    //   var blob = new Blob([action.payload], {
-    //     type: 'audio/wav',
-    //   });
-
-    //   var url = window.URL.createObjectURL(blob);
-    //   state.mp3File2 = url;
-    // },
-    // GET_MP3_FAILURE: (state, action: PayloadAction<any>) => {
-    //   state.error = action.payload;
-    // },
     SUBMIT_TTS_FAILURE: (state, action: PayloadAction<any>) => {
       state.error = action.payload;
     },
   },
 });
-
-export const { inputText, initialText, changeType } = ttsSlice.actions;
-
-export default ttsSlice.reducer;
+export const {
+  inputText,
+  initialText,
+  changeType,
+  SUBMIT_TTS_SUCCESS,
+} = ttsSlice.actions;
