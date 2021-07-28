a33641abc3da082621918571be55538712535d23
ww8007 <wshmin@naver.com>
2021-07-29 02:55:30 +0900

    fix : getMusic api

--- frontend/lib/api/music.ts
+++ frontend/lib/api/music.ts
@@ -1,11 +1,8 @@
 import client from './client';
 
-export const getMusic = ({title}) => {
-    const headers = {
-        'Content-type': 'text/html; charset=UTF-8',
-      };
-      // const Accept = '*/*';
-      const data = JSON.stringify({ title: title});
-    client.get(`/SVS`);
-
+export const getMusic = (title) => {
+  const headers = {
+    'Content-type': 'text/html; charset=UTF-8',
+  };
+  client.get(`/SVS?title=${title}`, { headers });
 };
