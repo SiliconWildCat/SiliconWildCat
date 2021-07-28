4eabb081faf1a60af97c463e5165bee6bb1b3969
ww8007 <wshmin@naver.com>
2021-07-28 17:18:51 +0900

    refactor : optimize useCallback, useMemo fix : select change -> rerender

--- frontend/components/TTS/TTsSlider.tsx
+++ frontend/components/TTS/TTsSlider.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import classNames from 'classnames';
 import { submit, tts, submitTTS, setInitialText } from '../../modules/tts';
 import Link from 'next/link';
@@ -7,14 +7,13 @@ import { useAppDispatch, useAppSelector } from '../../hooks/useSelector';
 import { RootState } from '../../modules';
 import { initialText, inputText, changeType } from '../../modules/tts';
 import { changeSelect } from '../../modules/music';
-import Loader from 'react-loader-spinner';
 import LoadingProgress from '../../lib/Loading/LoadingProgress';
 const options = [
   { value: 'KSS', label: 'KSS', key: 0 },
   { value: 'TaeYeon', label: 'TaeYeon', key: 1 },
 ];
 
-export default function TTsSlider({ Select }) {
+function TTsSlider({ Select }) {
   const { text, mp3File, type, loadingWAV } = useAppSelector(
     ({ tts, loading }: RootState) => ({
       text: tts.text,
@@ -37,19 +36,13 @@ export default function TTsSlider({ Select }) {
   });
 
   const dispatch = useAppDispatch();
-  const onSubmitText = (e) => {
-    e.preventDefault();
-    const info: submit = { text: text, type: type };
-    dispatch(submitTTS(info));
-    dispatch(initialText());
-    dispatch(setInitialText());
-  };
+
   useEffect(() => {
     window.clearTimeout(changeTO);
-  }, [changeTO, mp3File]);
+  }, [changeTO]);
   useEffect(() => {
     dispatch(setInitialText());
-  }, []);
+  }, [dispatch, mp3File, value]);
   useEffect(() => {
     setTimeout(() => {
       onOne4({
@@ -63,13 +56,26 @@ export default function TTsSlider({ Select }) {
     const { label } = value;
     dispatch(changeType(label));
   }, [value, dispatch]);
+  const onSubmitText = useCallback(
+    (e) => {
+      e.preventDefault();
+      const info: submit = { text: text, type: type };
+      dispatch(submitTTS(info));
+      dispatch(initialText());
+      dispatch(setInitialText());
+    },
+    [dispatch, text, type]
+  );
 
-  const onChangeText = (e) => {
-    dispatch(inputText(e.target.value));
-  };
-  const onInitial = () => {
+  const onChangeText = useCallback(
+    (e) => {
+      dispatch(inputText(e.target.value));
+    },
+    [dispatch]
+  );
+  const onInitial = useCallback(() => {
     dispatch(changeSelect(0));
-  };
+  }, [dispatch]);
 
   return (
     <>
@@ -150,13 +156,6 @@ export default function TTsSlider({ Select }) {
                   </div>
                 ))}
               </div>
-              {/* <Loader
-                type="Oval"
-                color="black"
-                height={30}
-                width={30}
-                timeout={3000}
-              ></Loader> */}
             </div>
           ))}
         </div>
@@ -167,3 +166,5 @@ export default function TTsSlider({ Select }) {
     </>
   );
 }
+
+export default React.memo(TTsSlider);
