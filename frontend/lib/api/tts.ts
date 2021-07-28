b8e1aa6878fbd9cafba8c1ceffa5e1963cc56e61
thfla1105 <lsr_1105@naver.com>
2021-07-28 17:04:56 +0900

    fix: setinitaltext 추가

--- frontend/lib/api/tts.ts
+++ frontend/lib/api/tts.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
-import { submit } from '../../modules/tts';
+import { setInitialText, submit } from '../../modules/tts';
 import client from './client';
 export const submitTTS = ({ text, type }: submit) => {
   const headers = {
     'Content-type': 'application/json; charset=UTF-8',
   };
+  setInitialText;
   // const Accept = '*/*';
   const data = JSON.stringify({ speech: text, voices: type });
   return client.post(`/TTS`, data, { headers });
