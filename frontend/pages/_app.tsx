e2b9f2fb6da7355b65b43dcadcd22a5b3c636fcf
ww8007 <wshmin@naver.com>
2021-07-29 03:37:14 +0900

    move : css order

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,12 +3,10 @@ import wrapper from '../store';
 import withReduxSaga from 'next-redux-saga';
 import '../styles/playerStyles.css';
 import '../styles/togglemenu.css';
-import '../styles/background.css';
 import '../components/Music/music.scss';
 import '../components/TTS/tts.scss';
 import '../styles/MusicPlayer3.css';
-
-
+import '../styles/background.css';
 
 function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
