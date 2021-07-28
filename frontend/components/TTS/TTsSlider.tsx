268d457bc8c9d6434cd2f8bdab2ac7d50c6cd5b5
Dudu-CNU <k0s0a7@gmail.com>
2021-07-28 17:17:15 +0900

    design : 페이지 이동버튼 fixed 적용

--- frontend/components/TTS/TTsSlider.tsx
+++ frontend/components/TTS/TTsSlider.tsx
@@ -101,9 +101,7 @@ export default function TTsSlider({ Select }) {
                 >
                   목소리를 선택하세요.
                 </h3>
-                <Link href="/music">
-                  <p className="slider__slide-readmores">go to music page</p>
-                </Link>
+                
                 <Select
                   className="slider__slide-subheadingss"
                   options={options}
@@ -157,9 +155,14 @@ export default function TTsSlider({ Select }) {
             </div>
           ))}
         </div>
-        <Link href="/music">
-          <a onClick={onInitial} className="slider__control" />
-        </Link>
+        <div className="slide_btns">
+          <Link href="/music">
+                  <p className="slider__slide-readmores" style={{position:'fixed'}}>go to music page</p>
+          </Link>
+          <Link href="/music">
+            <a onClick={onInitial} className="slider__control" />
+          </Link>
+        </div>
       </div>
     </>
   );
