95576507a12daf078e96e3875b4429bd0527d3c1
ww8007 <wshmin@naver.com>
2021-07-29 02:57:39 +0900

    delete : trash api

--- frontend/lib/api/tts.ts
+++ frontend/lib/api/tts.ts
@@ -10,17 +10,3 @@ export const submitTTS = ({ text, type }: submit) => {
   const data = JSON.stringify({ speech: text, voices: type });
   return client.post(`/TTS`, data, { headers });
 };
-
-export const getMP3 = () =>
-  axios.get(
-    `https://ww8007bucket.s3.ap-northeast-2.amazonaws.com/%ED%83%9C%EC%97%B0/y2mate.com+-+MV+Lee+Mujin%EC%9D%B4%EB%AC%B4%EC%A7%84++Traffic+light%EC%8B%A0%ED%98%B8%EB%93%B1.mp3`
-  );
-
-export const setTime = () => {
-  var _promise = new Promise(function (resolve, reject) {
-    setTimeout(function () {
-      resolve('22');
-    }, 3000);
-  });
-  return _promise;
-};
