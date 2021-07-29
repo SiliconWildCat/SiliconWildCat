a3cef7b454309574e00c12b5a67895d9f1454c55
ww8007 <wshmin@naver.com>
2021-07-29 12:19:29 +0900

    fix : get wavFile from server

--- frontend/lib/api/music.ts
+++ frontend/lib/api/music.ts
@@ -4,5 +4,5 @@ export const getMusic = (title) => {
   const headers = {
     'Content-type': 'text/html; charset=UTF-8',
   };
-  client.get(`/SVS?title=${title}`, { headers });
+  return client.get(`/SVS?title=${title}`, { headers });
 };
